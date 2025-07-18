6e79db73c3b163cba45eaefe4dd8646c98110c29
albert <gatx105tw@yahoo.com.tw>
2025-07-18 14:56:09 +0800

    feat: 更新單元測試以反映正確的標題 '漂漂河小遊戲'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-angular-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('漂漂河小遊戲');
   });
 });
