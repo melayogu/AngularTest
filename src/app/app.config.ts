dd977fe0b20bd21b6ad7985c7eee3edf6da36695
albert <gatx105tw@yahoo.com.tw>
2025-07-23 13:49:09 +0800

    feat: 移除路由配置中的初始導航阻塞選項，更新基礎路徑設定

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withEnabledBlockingInitialNavigation())
+    provideRouter(routes)
   ]
 };
