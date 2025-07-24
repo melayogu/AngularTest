0e3250cd973e8f4cee86362dbedbffcdc2367291
albert <gatx105tw@yahoo.com.tw>
2025-07-24 14:23:26 +0800

    feat: 重構電梯遊戲組件，新增樣式與邏輯，改善顯示與控制按鈕

--- src/app/components/elevator-game/elevator-game.component.ts
+++ src/app/components/elevator-game/elevator-game.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-elevator-game',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './elevator-game.component.html',
   styleUrls: ['./elevator-game.component.css']
 })
 export class ElevatorGameComponent {
-  // 遊戲邏輯將在此實作
   currentFloor = 1;
   maxFloor = 10;
   minFloor = 1;
+  floors: number[] = [];
+
+  constructor() {
+    // 由高到低顯示樓層
+    for (let i = this.maxFloor; i >= this.minFloor; i--) {
+      this.floors.push(i);
+    }
+  }
 
   goUp() {
     if (this.currentFloor < this.maxFloor) {
